e1ee584509bb36dba214afbf24e5ad51ee12d705
mlamamra <mlamamra@dawan.fr>
2026-02-23 17:33:03 +0100

    fonctions lambdas: map & filter & tri & reduction

--- rappels.py
+++ rappels.py
@@ -26,3 +26,88 @@ print(f"""
 # contenu de type 'page html' donc texte
 if "text/html" in response.headers["Content-Type"]:
   print(response.text)
+
+# %%
+# gestion des paramètres d'une fonction
+## paramètres positionnels: appelés de manière nommée
+
+def substr(x: int, y: int) -> int:
+  return x - y
+
+print(substr(2, 3))
+print(substr(3, 2))
+# les annotations sont informatives, pas de contrôle de type
+print(substr.__annotations__)
+# TypeError: print(substr("machin", 2))
+
+# les valeurs de paramètre sont fléchées
+print(substr(y=2, x=3))
+
+# %%
+from functools import reduce
+
+# gestion des paramètres d'une fonction
+## paramètres "variadics" positionnels: *args
+
+
+# je veux sommer un nb indéfini de nombres
+def sum_all(*truc: float) -> float:
+  print(truc)
+  return reduce(lambda acc, x: acc + x, truc)
+
+print(sum_all(1, 2, 3))
+
+# %%
+
+from datetime import datetime
+import re
+
+# gestion des paramètres d'une fonction
+## paramètres "variadics" nommés: **kwargs
+
+def create_user(name: str, date_birth: str, **opts):
+  """
+  fonction de création d'utilisateur
+  name: nom de l'utilisateur
+  date_birth: date de naissance de l'utilisateur
+  opts: options supplémentaires
+    - email
+    - age
+    ...
+  """
+  dt = datetime.strptime(date_birth, "%Y-%m-%d")
+  # partie obligatoire
+  user = {
+    "name": name,
+    "date_birth": dt.strftime("%Y-%m-%d")
+  }
+  # r"" : raw string, pour ne pas devoir échapper les caractères spéciaux
+  # [^@]: tout caractère sauf @
+  # + : un ou plusieurs caractères
+  # \. : le caractère . car "." signifie n'importe quel caractère en regex
+  if "email" in opts:
+    if re.match(r"[^@]+@[^@]+\.[^@]+", opts["email"]):
+      user["email"] = opts["email"]
+    else:
+      raise ValueError(f"email {opts['email']} non valide")
+  return user
+
+try:
+  print(create_user("John Doe", "1981-05-22"))
+  print(create_user("John Doe", "1981-05-22", email="joe@example.com", truc="machin"))
+except ValueError as e:
+  print(f"Erreur: {e}")
+
+# %%
+## utilisation de * et ** à l'appel d'une fonction
+
+parameters = ["John Doe", "1981-05-22"]
+print(create_user(parameters[0], parameters[1]))
+# injecter les éléments de la liste 
+# dans les paramètres positionnels de la fonction
+print(create_user(*parameters))
+
+kwds = {"name": "John Doe", "date_birth": "1981-05-22"}
+# les clés du dictionnaire doivent correspondre aux noms des paramètres de la fonction
+print(create_user(**kwds))
+# %%
