f9f31763a625d890f47c808728806dde57dc0fb3
mlamamra <mlamamra@dawan.fr>
2026-02-24 14:52:09 +0100

    logging: renommage + executer le prog principal

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ pour enregistrer des événements dans le programme (résultats attendus, erreur
 # import imported_module
 
 import logging
+# import avec espace de nom => protection contre les collisions de nom
+import crypto
+# import sans espace de nom => plus court mais collision possible
+# from crypto import brute_force
+# from crypto import brute_force as bf => alias pour gérer une collision
+
 # configurer le fichier de log ici
 # niveaux de sevérité: debug, info, warning, error, critical
 # format
@@ -18,6 +24,8 @@ logging.basicConfig(
   format="%(asctime)s - %(levelname)s : %(message)s"
 )
 
+# collision possible
+# brute_force = 2
 
 if __name__ == "__main__":
   print(f"nom du module principal: {__name__}")
@@ -27,3 +35,6 @@ if __name__ == "__main__":
   ## dir affiche tous les attributs d'une variable
   ## print(dir(imported_module))
   ## print(f"nom du module importé: {imported_module.__name__}")
+  secret = "3ed7dceaf266cafef032b9d5db224717"
+
+  print(crypto.brute_force(secret))
