0f839ed1072f90ad3119b3b34eb9f7f7a28295c8
mlamamra <mlamamra@dawan.fr>
2026-02-24 14:42:47 +0100

    logging: ajout du module crypto

---
+++ crypto.py
@@ -0,0 +1,15 @@
+from hashlib import md5
+from string import ascii_lowercase
+from time import time
+import itertools
+
+def sign(target: str) -> str:
+  octets = bytes(target, "utf8")
+  return md5(octets).hexdigest()
+
+def brute_force(secret: str):
+  for i in itertools.count(1):
+    for tup in itertools.product(ascii_lowercase, repeat=i):
+      word = "".join(tup)
+      if sign(word) == secret:
+        return word
