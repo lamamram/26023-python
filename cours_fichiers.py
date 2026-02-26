1ff734323359e4ee9475c7f09c7a951311d1f618
mlamamra <mlamamra@dawan.fr>
2026-02-26 15:53:49 +0100

    fichiers: csv

--- cours_fichiers.py
+++ cours_fichiers.py
@@ -48,3 +48,30 @@ with open("data.txt", "r", encoding="utf-8") as f:
   for line in f:
     print(line, end="") # end="" => ne pas ajouter de saut de ligne après chaque ligne lue
 # %%
+## écriture d'un csv
+import csv
+
+# csv.reader() # pour lire un csv
+# csv.writer() # pour écrire dans un csv
+
+users = [
+  {"id": 1, "first_name": "Jean", "last_name": "Dupont", "age": 30, "comment": "blabla; blabla"},
+  {"id": 2, "first_name": "Marie", "last_name": "Curie", "age": 25, "comment": "blabliblo"},
+]
+
+# newline="" => ne pas ajouter de ligne vide entre chaque ligne écrite dans le csv avec Windows
+with open("data.csv", "w", encoding="utf-8", newline="") as f:
+  # quotechar='"' => les champs contenant des caractères spéciaux (ex: ;) sont entourés de guillemets
+  # quoting=csv.QUOTE_MINIMAL => n'entoure que les champs qui en ont besoin
+  writer = csv.writer(f, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+  header = users[0].keys()
+  writer.writerow(header)
+  for user in users:
+    writer.writerow(user.values())
+  
+  ## option B: transfotmat° d'une liste de dicts en une liste de listes de valeurs
+  # records = list(map(lambda user: list(user.values()), users))
+  # writer.writerows(records)
+
+
+# %%
